e6e68db052d55e0715e24d31ccb475d058cc125c
dwqs <461147874@qq.com>
2016-11-06 15:07:00 +0800

    float right completed

--- client/components/FloatRight.vue
+++ client/components/FloatRight.vue
@@ -1,12 +1,43 @@
 <template>
-    <div class="float-right">
-        {{msg}}
-    </div>
+    <aside class="float-right">
+        <ul>
+            <li>
+                <a href="https://github.com/dwqs/vue-resume.git" :target="target">源代码</a>
+            </li>
+            <li>
+                <a href="https://github.com/dwqs" :target="target">Github</a>
+            </li>
+            <li>
+                <a href="https://github.com/dwqs/blog" :target="target">Blog</a>
+            </li>
+            <li>
+                <a href="#" :target="target">PDF下载</a>
+            </li>
+        </ul>
+    </aside>
 </template>
 
 <style scoped lang="less">
     .float-right{
-        color:blue
+        position: fixed;
+        right: 0;
+        top: 30%;
+        z-index: 2;
+        li{
+            margin-bottom: 10px;
+            border-radius: 10px 0 0 10px;
+            box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12);
+            background-color: #00BFA5;
+            cursor: pointer;
+            a{
+                display: inline-block;
+                width: 80px;
+                height: 38px;
+                line-height: 38px;
+                padding-left: 10px;
+                color: #FFFFFF;
+            }
+        }
     }
 </style>
 
@@ -15,7 +46,7 @@
     export default{
         data (){
             return{
-                msg:'float-right'
+                target:'_blank'
             };
         }
     };
