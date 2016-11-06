da5000cbdffd60cd6309c322cd802617b3349f8a
dwqs <461147874@qq.com>
2016-11-06 17:07:32 +0800

    font awesome

--- client/components/ResumeHeader.vue
+++ client/components/ResumeHeader.vue
@@ -1,12 +1,27 @@
 <template>
     <header class="resume-header">
-        {{msg}} - {{data.age}}
+        {{msg}} - {{data.age}}11
     </header>
 </template>
 
 <style scoped lang="less">
     .resume-header{
-        color:blue
+        color:blue;
+        position: relative;
+    }
+    .resume-header:before {
+        content: "\f000";
+        font-family: FontAwesome;
+        font-style: normal;
+        font-weight: normal;
+        text-decoration: inherit;
+        /*--adjust as necessary--*/
+        /*color: red;*/
+        /*font-size: 18px;*/
+        /*padding-right: 0.5em;*/
+        /*position: absolute;*/
+        /*top: 10px;*/
+        /*left: 0;*/
     }
 </style>
 
