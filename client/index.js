56b538c2239b3819cb7630b65dfe473450ab3e59
dwqs <461147874@qq.com>
2016-11-10 01:15:07 +0800

    fix print

--- client/index.js
+++ client/index.js
@@ -6,7 +6,7 @@
 
 import './styles/reset.css';
 import './styles/common.css';
-import './styles/print.less';
+import './styles/print.css';
 //require('./common./css|less/');
 //import './xxx.less';
 
