21b808c9eec7cf4dca0760212f21d0535e289514
dwqs <461147874@qq.com>
2016-11-13 00:40:58 +0800

    download pdf

--- client/components/FloatRight.vue
+++ client/components/FloatRight.vue
@@ -61,7 +61,7 @@
                     isPrint:false
                 },{
                     text:"PDF下载",
-                    link:"#",
+                    link:"/袁康-个人简历-前端开发.pdf",
                     isPrint:false
                 },{
                     text:"打印简历",
