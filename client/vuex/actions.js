92d6c84f1e4c4643fda725ae4b28a64c43b7e2a7
dwqs <461147874@qq.com>
2016-11-15 01:47:45 +0800

    replace fetch with xhr

--- client/vuex/actions.js
+++ client/vuex/actions.js
@@ -11,12 +11,27 @@ export const beforeGetResumeData = (store) => {
 
 export const getResumeData = ({commit,dispatch}) => {
     dispatch('beforeGetResumeData'); //same as commit(types.BEFORE_GET_RESUME_DATA)
-    fetch('/resume').then((res)=>{
-        return res.json();
-    }).then((data)=>{
-        commit({
-            type:types.GET_RESUME_DATA,
-            resume:data.resume
-        });
-    }).catch((err) => console.log('error',err));
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () =>{
+        if(xhr.readyState === 4){
+            if(xhr.status >= 200 && xhr.status < 300){
+                commit({
+                    type:types.GET_RESUME_DATA,
+                    resume:JSON.parse(xhr.responseText).resume
+                });
+            } else {
+                console.log('request was unsuccessful:',xhr.status);
+            }
+        }
+    };
+    xhr.open('GET','/resume',true);
+    xhr.send(null);
+    // fetch('/resume').then((res)=>{
+    //     return res.json();
+    // }).then((data)=>{
+    //     commit({
+    //         type:types.GET_RESUME_DATA,
+    //         resume:data.resume
+    //     });
+    // }).catch((err) => console.log('error',err));
 };
